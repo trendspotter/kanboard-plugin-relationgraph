59c360efd2153a329f7898c8de836f572c4f2041
Claudio Ferreira <filhocf@gmail.com>
2017-12-10 14:30:19 -0200

    Added Brazilian portuguese translation.

---
+++ Locale/pt_BR/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'Show relations between tasks using a graph' => 'Mostra as relações entre as tarefas usando um grafo',
+    'Relation graph' => 'Grafo de relações',
+    'Project: ' => 'Projeto: ',
+    'Score: ' => 'Pontuação: ',
+    'Assignee: ' => 'Designador: ',
+    'Priority: ' => 'Prioridade: ',
+    'Column: ' => 'Coluna: ',
+);

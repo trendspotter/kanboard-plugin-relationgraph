d29fd3617d2157a30cacd5dc64508e6bd3fa8ef0
trendspotter <j.podhorecky@volny.cz>
2020-04-18 18:10:19 +0200

    Add cs_CZ translations

---
+++ Locale/cs_CZ/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'Show relations between tasks using a graph' => 'Zobrazit vztahy mezi úkoly pomocí grafu',
+    'Relation graph' => 'Graf vztahů',
+    'Project: ' => 'Projekt: ',
+    'Score: ' => 'Skóre: ',
+    'Assignee: ' => 'Přihlašovatel: ',
+    'Priority: ' => 'Priorita: ',
+    'Column: ' => 'Sloupec: ',
+);
